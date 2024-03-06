58b572d84ddca222b52ebdec8007c30897b3df39
YukthaJayagopal <162403809+YukthaJayagopal@users.noreply.github.com>
2024-03-06 13:50:44 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "WEBHOOK\n";
+  cout << "Webhook\n";
   return 0;
 }
